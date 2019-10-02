a51d795351fc08250c00cce9dc22bdd948b5ffa3
Evelyn Le <48920501+EvelynLe1024@users.noreply.github.com>
2019-10-01 21:37:18 -0400

    Update and rename MaximumDepthofBinaryTree.py to maximum_depth_of_binary_tree.py

--- maximum_depth_of_binary_tree.py
+++ maximum_depth_of_binary_tree.py
@@ -31,4 +31,6 @@ class Solution:
             return 0 
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
-        return 1 + max(left, right)      
+        return 1 + max(left, right) 
+ # Time complexity: O(N) 
+ # Space complexity: O(1)
