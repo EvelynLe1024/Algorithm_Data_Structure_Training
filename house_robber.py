01aff1f2e54c7988f6c84914b3c4fffb95060821
Evelyn Le <48920501+EvelynLe1024@users.noreply.github.com>
2019-10-01 21:40:54 -0400

    Update and rename HouseRobber.py to house_robber.py

--- house_robber.py
+++ house_robber.py
@@ -40,4 +40,7 @@ class Solution:
         for i in range(2, n):
             h[i] = max(nums[i] + h[i - 2], h[i - 1])
         return h[n - 1]
+      
+ # Time complexity: O(N)
+ # Space complexity: O(N)
  
