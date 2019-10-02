705603c91132d607c500c6d78c4b60d92863e69c
Evelyn Le <48920501+EvelynLe1024@users.noreply.github.com>
2019-10-02 14:10:52 -0400

    Update move_zeros.py

--- move_zeros.py
+++ move_zeros.py
@@ -12,10 +12,11 @@ Minimize the total number of operations.
 Source: https://leetcode.com/problems/move-zeroes/
 
 *** 
+Solution:
 The solution below uses two pointers i and j that are initialized to 0 to modify the array in-place. It uses a while loop to iterate 
-through the input array. i points to the first index in the array that hasn't been modified. j iterates throuhg the list. 
-Whenever an element at j is different from 0, we swap the two elements at i and j and increment both pointers by 1. If an element is 0, 
-j is incremented by 1. 
+through the input array. i points to the first index in the array that hasn't been modified. j iterates through the list to find
+elements that are different from 0 to swap with i. Whenever an element at j is different from 0, we swap the two elements at i and j 
+and increment both pointers by 1. If an element is 0, j is incremented by 1. 
 
 '''
 class Solution:
