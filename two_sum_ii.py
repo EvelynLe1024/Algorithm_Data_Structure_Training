834f30266376987ccf28cbde91db314fa9f86865
Evelyn Le <48920501+EvelynLe1024@users.noreply.github.com>
2019-10-04 11:18:31 -0400

    Update two_sum_ii.py

--- two_sum_ii.py
+++ two_sum_ii.py
@@ -18,7 +18,7 @@ I present here two different solutions to this problem.
 
 '''
 I. Solution 1: Greedy 
-The first solution uses the greedy algorithm to find the desired indices. The greedy althorithm computes a solution in steps. At each step,
+The 1st solution uses the greedy algorithm to find the desired indices. The greedy althorithm computes a solution in steps. At each step,
 the algorithm picks a locally optimum option. The solution takes advantage of the sortedness of the array to:
 1. use a while loop with two pointers i and j to iterate through the array. Pointer i starts at index 0, and pointer j starts at the last 
 index of the array. 
@@ -40,4 +40,37 @@ class Solution:
             else:
                 i += 1
     # Time complexity: O(N)
+    # Space complexity: O(1)        
+'''
+II. Solution 2: Binary Search 
+The 2nd solution also takes advantage of the sortedness of the array to find the desired indices. It: 
+1. uses a helper function to do a binary search on an array, returning the index of the element equal to target  
+2. iterates through the input array to find the index of the element that can be added to the currrent index to equal the target.
+
+Solution 2 is less efficient than solution 1 since we perform a binary search at each iterated index in the array, leading to a time 
+complexity of O(NlogN) compared to O(N) in solution 1. 
+'''
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        if not numbers:
+            return
+        for i in range(len(numbers) - 1):
+            comp = target - numbers[i]
+            if self.helperBS(numbers[i:], comp):
+                return [i + 1, self.helperBS(numbers[i:], comp) + i + 1]
+    
+    def helperBS(self, nums, tar):
+        lo, hi = 0, len(nums) - 1        
+        while lo + 1 < hi:
+            if nums[mid] > tar: 
+                hi = mid
+            else:
+                lo = mid
+        if nums[lo] == tar:
+            return lo
+        elif nums[hi] == tar:
+            return hi 
+        return  
+
+    # Time complexity: O(NlogN)
     # Space complexity: O(1)
