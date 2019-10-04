9948be4221325e3d4c2db9fcd0630e151a71ad11
Evelyn Le <48920501+EvelynLe1024@users.noreply.github.com>
2019-10-04 12:08:32 -0400

    Update two_sum_ii.py

--- two_sum_ii.py
+++ two_sum_ii.py
@@ -11,6 +11,8 @@ Input: numbers = [2,7,11,15], target = 9
 Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 
+Source: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
 ***
 Solution
 I present here two different solutions to this problem. 
